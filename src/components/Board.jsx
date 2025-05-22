12c2bf187bb74a20ba30990eaf3a6f67e569b894
Joshua Sigston <joshua.sigston@outlook.com>
2025-05-21 17:42:58 -0700

    game finished

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Square from "./Square";
 
-const Board = ({ square, onClick }) => (
-  <div className="">
-    <Square value="1" onClick={() => onClick("o")} />
-    <Square value="2" onClick={() => onClick("o")} />
-    <Square value="3" onClick={() => onClick("o")} />
-    <Square value="4" onClick={() => onClick("o")} />
-    <Square value="5" onClick={() => onClick("o")} />
-    <Square value="6" onClick={() => onClick("o")} />
-    <Square value="7" onClick={() => onClick("o")} />
-    <Square value="8" onClick={() => onClick("o")} />
-    <Square value="9" onClick={() => onClick("o")} />
+const style = {
+  border: "4px solid darkblue",
+  borderRadius: "10px",
+  width: "250px",
+  height: "250px",
+  margin: "0 auto",
+  display: "grid",
+  gridTemplate: "repeat(3, 1fr) / repeat(3, 1fr)",
+};
+
+const Board = ({ squares, onClick }) => (
+  <div style={style}>
+    {squares.map((square, i) => (
+      <Square key={i} value={square} onClick={() => onClick(i)} />
+    ))}
   </div>
 );
 export default Board;
